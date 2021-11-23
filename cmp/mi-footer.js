eeade92e8ebd07a3b38bbb18c580d73e036acc01
WebApp6 <webappwano@gmail.com>
2021-11-23 13:08:58 -0600

    adecuaciones

---
+++ cmp/mi-footer.js
@@ -0,0 +1,13 @@
+class MiFooter
+  extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = /* html */
+      `<p>
+        &copy; 2021
+        Juan Fernando Martinez Bojorges.
+      </p>`;
+  }
+}
+
+customElements.define(
+  "mi-footer", MiFooter);
---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyB6SM02ADGx4D0RYR3hzI6GaOC1_vgHjUg",
+  authDomain: "fbbd-e5598.firebaseapp.com",
+  projectId: "fbbd-e5598",
+  storageBucket: "fbbd-e5598.appspot.com",
+  messagingSenderId: "962833400116",
+  appId: "1:962833400116:web:075b650fa0188abe9da561",
+  measurementId: "G-C2MLZE25VH"
+});
